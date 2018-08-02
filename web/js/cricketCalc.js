0b33a03dce520de093a5a72ff02621f24342e3bf
GeordieR <geordie@grogers.xyz>
2018-08-02 19:04:22 +1200

    Fixed no runs turns to NaN

--- web/js/cricketCalc.js
+++ web/js/cricketCalc.js
@@ -18,10 +18,12 @@ function enter(key) {
 */
 function backspace() {
     var previousBall = scorecard.charAt(scorecard.length - 1);
-    score = score - previousBall;
+    if(previousBall!='.'){
+        score = score - previousBall;
+        document.getElementById('score').innerHTML = score;
+    }
     scorecard = scorecard.substring(0, scorecard.length-1);
     document.getElementById('scorecard').innerHTML = scorecard;
-    document.getElementById('score').innerHTML = score;
 }
 
 function reset() {
